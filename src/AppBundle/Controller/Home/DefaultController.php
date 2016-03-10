a4df08b574ff215d42b7572e5b82223a12f2122a
Jakub Polák <jakub.polak@me.com>
2016-03-10 18:07:24 +0100

    Various.

---
+++ src/AppBundle/Controller/Home/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Default controller.
+ *
+ * @author Jakub Polák, Jana Poláková
+ */
+class DefaultController extends Controller {
+    
+}
